2536f11ac834051736b9ac25c32e6f639554c225
Sreekanth Josyula <sreekanthjo@gmail.com>
2017-10-11 13:42:16 +0530

    fixing coments

--- Task1/TransportationOnVacation.js
+++ Task1/TransportationOnVacation.js
@@ -1,10 +1,9 @@
-function rentalCarCost(d) {  
-   var dayCost=40;
-   var totalCost = (d * dayCost);
+function rentalCarCost(d) {
+    var dayCost = 40;
+    var totalCost = (d * dayCost);
 
-    if(d>=7)
-    totalCost -= 50;
-        if(d>=3)
-        totalCost -= 20;
+    if (d >= 7) { totalCost -= 50; }
+    else if (d >= 3) { totalCost -= 20; }
+
     return totalCost;
-  }
+}
