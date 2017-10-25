779b2dd85017e3e28ed585129aed9877d40fa7a5
SreekanthJos <sreekanthjo@gmail.com>
2017-10-25 17:27:42 +0530

    Update WordCount.js

--- Task2/WordCount.js
+++ Task2/WordCount.js
@@ -1,6 +1,6 @@
 function countWords(str) {
-  var words=str.trim().split(/\s+/);
-  var result=words.filter(
+  var words=str.trim().split(/\s+/),
+  result=words.filter(
     function(val){
       return val;
     });
