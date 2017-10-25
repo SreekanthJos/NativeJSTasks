a0e96b239550ce5d4d354d4fb2b28d836dbb35b1
SreekanthJos <sreekanthjo@gmail.com>
2017-10-25 17:15:06 +0530

    Update PartionOn.js

--- Task2/PartionOn.js
+++ Task2/PartionOn.js
@@ -2,7 +2,7 @@
 // at the end, returning the index of the first true value
 function partitionOn(pred, items) {
 var falseCollection=items.filter(function(i){return !pred(i);});
-var truthCollection=items.filter(function(i){return pred(i);});
+var truthCollection=items.filter(pred);
 items.length=0;
 items.push.apply(items,falseCollection.concat(truthCollection));
 return falseCollection.length;
