bd8b83dd9601e39be39bb7d8651f456e01ffb0b1
SreekanthJos <sreekanthjo@gmail.com>
2017-10-25 17:26:46 +0530

    Update GetTheMiddleCharacter.js

--- Task2/GetTheMiddleCharacter.js
+++ Task2/GetTheMiddleCharacter.js
@@ -1,6 +1,6 @@
 function getMiddle(s)
 {
-var res = (s.length%2);
-var midIndex = s.length/2;
+var res = (s.length%2),
+midIndex = s.length/2;
 return res===0 ? s.substring(midIndex-1,midIndex+1) : s.substring(midIndex,midIndex+1);
 }
