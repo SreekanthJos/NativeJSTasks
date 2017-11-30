0f64f58c2dc7841d78989a8a8b9e956a8fd628a5
Sreekanth Josyula <sreekanthjo@gmail.com>
2017-11-30 22:21:54 +0530

    added api.js

--- Project/index.js
+++ Project/index.js
@@ -1,45 +1,22 @@
-var searchBox = document.createElement("input")
-searchBox.setAttribute("type", "text");
-searchBox.setAttribute("id", "txtSearch");
+//'use strict';
+(function () {
+    var searchBox = document.createElement("input")
+    searchBox.setAttribute("type", "text");
+    searchBox.setAttribute("id", "txtSearch");
+    document.getElementById("main").appendChild(searchBox);
 
-document.getElementById("main").appendChild(searchBox);
-
-
-
-document.getElementById("txtSearch").onchange = function () {
-
-    getData().then(function(data){
-        console.log(data);
-    })
-}
-
-
-getData = function (searchVal) {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        var url = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyCTWC75i70moJLzyNh3tt4jzCljZcRkU8Y&type=video&part=snippet&maxResults=15&q=js';
-        xhr.open('GET', url);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            }
-            else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            }
-        };
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
+    document.getElementById("txtSearch").onchange = function () {
+        getDataForVideos(this.value).then(function (data) {
+            searchResults = JSON.parse(data);            
+            getVideoStatistics(searchResults).then(function (response) {
+                stats=JSON.parse(response);
+                processData();
             });
-        };
-xhr.send();
+        })
+    }
 
-    });
+})();
 
-}
+
 
 
